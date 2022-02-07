f5c9aaaf9046c1beff7f353e116326652756ddc4
Marno <git@techroose.com>
2022-02-07 18:35:18 +0000

    Corrected grammar in about

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,13 +9,13 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is Chess version of the Wordle game{' '}
+        This is a Chess-themed version of Wordle{' '}
         <a
           href="https://github.com/marnovandermaas/chessle"
           className="underline font-bold"
         >
           check out the code here
-        </a>{' '}
+        </a>.{' '}
         It is inspired by{' '}
         <a
           href="https://www.powerlanguage.co.uk/wordle/"
@@ -23,20 +23,20 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
         >
           the original Wordle
         </a>{' '}
-        by{' '}
+        and the bike-themed version{' '}
         <a
           href="https://github.com/giop98/bikle"
           className="underline font-bold"
         >
           Bikle
-        </a>{' '}
-        and the code is based on{' '}
+        </a>.{' '}
+        The code is based on{' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          Reactle.
-        </a>{' '}
+          Reactle
+        </a>.{' '}
       </p>
     </BaseModal>
   )
