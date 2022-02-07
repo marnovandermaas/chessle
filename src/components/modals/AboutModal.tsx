dcc4711c91d2a2078c98d4d3561e3924ac949c45
Marno <git@techroose.com>
2022-02-07 14:23:07 +0000

    Updated about model, index, manifest and strings for Chessle

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,13 +9,33 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        This is Chess version of the Wordle game{' '}
         <a
           href="https://github.com/marnovandermaas/chessle"
           className="underline font-bold"
         >
           check out the code here
+        </a>{' '}
+        It is inspired by{' '}
+        <a
+          href="https://www.powerlanguage.co.uk/wordle/"
+          className="underline font-bold"
+        >
+          the original Wordle
+        </a>{' '}
+        by{' '}
+        <a
+          href="https://github.com/giop98/bikle"
+          className="underline font-bold"
+        >
+          Bikle
+        </a>{' '}
+        and the code is based on{' '}
+        <a
+          href="https://github.com/cwackerfuss/react-wordle"
+          className="underline font-bold"
+        >
+          Reactle.
         </a>{' '}
       </p>
     </BaseModal>
